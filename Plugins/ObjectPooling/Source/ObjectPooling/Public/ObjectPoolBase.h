990afa55c85de09f28634f65459f387f412a5582
Leandro <leandro@italiangamesfactory.com>
2026-01-12 17:13:20 +0100

    3 Unreal 2 & CppAdv
    - Aggiunto i seguenti plugin:
    - Content only
    - Codice: ObjectPooling example
    - Pathfinding 3d: plugin gratuito Customizable Pathfinding: https://www.fab.com/listings/4aafd8c4-b55c-49c9-8f9c-654f5b5b07c7
    Esempio di come compilare un plugin di una versione prima dell'engine

---
+++ Plugins/ObjectPooling/Source/ObjectPooling/Public/ObjectPoolBase.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class OBJECTPOOLING_API ObjectPoolBase
+{
+public:
+	ObjectPoolBase();
+	~ObjectPoolBase();
+};
---
+++ Source/Unreal2CPPEditor/Private/ObjectEditor.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ObjectEditor.h"
+
+ObjectEditor::ObjectEditor()
+{
+}
+
+ObjectEditor::~ObjectEditor()
+{
+}
