60d63e5a9b4ffbb948aaa9eb6f616a66dc1f51cc
Leandro <leandro@italiangamesfactory.com>
2026-02-16 15:58:57 +0100

    Redirector fix example

--- Plugins/VoxelGridPathfinding/Source/VoxelGridPathfinding/Private/VoxelGridSettings.cpp
+++ Plugins/VoxelGridPathfinding/Source/VoxelGridPathfinding/Private/VoxelGridSettings.cpp
@@ -6,3 +6,22 @@ UVoxelGridSettings::UVoxelGridSettings()
 	CategoryName = TEXT("Plugins");
 	SectionName = TEXT("Voxel Grid Pathfinding");
 }
+
+
+/*
+
+FPath::ProjectSaveDir() + TEXT("SaveGames/")
+
+FFileHelper::SaveStringToFile(String, *Path)
+
+FFileHelper::LoadFileToString(String, *fullpath)
+
+IPlatformFile::GetPlatformFile().CreateDirectoryTree(*Path)
+
+#include "JsonObjectConverter.h" 
+#include "Misc/FileHelper.h" 
+#include "Misc/Paths.h"
+
+FPlatformProcess::Sleep(2.0f)
+
+*/
